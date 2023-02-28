b0bbd981d5a518ca170ea77037a32aa88d744d0a
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-02-28 20:40:13 +0200

    file transfer

---
+++ src/redux/contacts/contacts-slice.js
@@ -0,0 +1,30 @@
+import { nanoid } from 'nanoid';
+import { createSlice } from '@reduxjs/toolkit';
+
+const contactsSlice = createSlice({
+  name: 'contacts',
+  initialState: [],
+  reducers: {
+    addContact: {
+      reducer: (store, { payload }) => {
+        store.push(payload);
+      },
+      prepare: data => {
+        return {
+          payload: {
+            ...data,
+            id: nanoid(),
+          },
+        };
+      },
+    },
+    removeContact: (store, { payload }) =>
+      store.filter(({ id }) => id !== payload),
+  },
+});
+
+export const { addContact, removeContact } = contactsSlice.actions;
+// console.log(addContact.type);
+export const contactsReducer = contactsSlice.reducer;
+
+// payload забираємо з action
