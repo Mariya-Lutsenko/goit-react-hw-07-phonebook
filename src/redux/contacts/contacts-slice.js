717828f25147e276ec4839bc2a34c5cec4942d8d
Mariya-Lutsenko <107764269+Mariya-Lutsenko@users.noreply.github.com>
2023-03-02 00:19:34 +0200

    extraReducers, builder

--- src/redux/contacts/contacts-slice.js
+++ src/redux/contacts/contacts-slice.js
@@ -11,44 +11,44 @@ const contactsSlice = createSlice({
   // name так будуть починатися actions
   name: 'contacts',
   initialState,
-  extraReducers: {
-    // contacts/fetch/pending
-    [fetchContacts.pending]: state => {
-      state.loading = true;
-      state.error = null;
-    },
-    [fetchContacts.fulfilled]: (state, { payload }) => {
-      state.loading = false;
-      state.items = payload;
-    },
-    [fetchContacts.rejected]: (state, { payload }) => {
-      state.loading = false;
-      state.error = payload;
-    },
-    [addContact.pending]: state => {
-      state.loading = true;
-      state.error = null;
-    },
-    [addContact.fulfilled]: (state, { payload }) => {
-      state.loading = false;
-      state.items.push(payload);
-    },
-    [addContact.rejected]: (state, { payload }) => {
-      state.loading = false;
-      state.error = payload;
-    },
-    [deleteContact.pending]: state => {
-      state.loading = true;
-      state.error = null;
-    },
-    [deleteContact.fulfilled]: (state, { payload }) => {
-      state.loading = false;
-      state.items = state.items.filter(item => item.id !== payload);
-    },
-    [deleteContact.rejected]: (state, { payload }) => {
-      state.loading = false;
-      state.error = payload;
-    },
+  extraReducers: builder => {
+    builder
+      .addCase(fetchContacts.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(fetchContacts.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.items = payload;
+      })
+      .addCase(fetchContacts.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(addContact.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(addContact.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.items.push(payload);
+      })
+      .addCase(addContact.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      })
+      .addCase(deleteContact.pending, state => {
+        state.loading = true;
+        state.error = null;
+      })
+      .addCase(deleteContact.fulfilled, (state, { payload }) => {
+        state.loading = false;
+        state.items = state.items.filter(item => item.id !== payload);
+      })
+      .addCase(deleteContact.rejected, (state, { payload }) => {
+        state.loading = false;
+        state.error = payload;
+      });
   },
 });
 
